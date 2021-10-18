9433567688644f7b62cd4853397bed0a780a0ae9
Hernan <53800192+hernantorres23@users.noreply.github.com>
2021-10-18 01:27:46 -0400

    Complete module

--- jobs/app.py
+++ jobs/app.py
@@ -48,9 +48,9 @@ def employer(employer_id):
     reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
 
-@app.route('/employer/<employer_id>/review')
+@app.route('/employer/<employer_id>/review', methods='GET', 'POST')
 def review(employer_id):
-    return render_template()
+    return render_template('review.html')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=true)
